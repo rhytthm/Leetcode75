ca80462189623a6686df89c53aaacaaaab8929a8
RAJEEV MAHAJAN <rajeevmahajan@RAJEEVs-MacBook-Air.local>
2025-02-23 18:05:40 +0530

    Creating Playground

---
+++ Leetcode.playground/Pages/Greatest Common Divisor of Strings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+ ## Greatest Common Divisor of Strings
+ For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+ Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+ ```
+ Input: str1 = "ABABAB", str2 = "ABAB"
+ Output: "AB"
+ ```
+ [**Link**](https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75)
+ */
+
+import Foundation
+
+class Solution {
+    func gcdOfStrings(_ str1: String, _ str2: String) -> String {
+        
+    }
+}

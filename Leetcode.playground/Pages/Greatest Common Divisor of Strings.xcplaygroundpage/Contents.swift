2d4c1d9a4be66b9221fafe9c730e45865a267768
Rhytthm <rhytthm1717@gmail.com>
2025-02-23 20:23:53 +0530

    gcdOfStrings solution

--- Leetcode.playground/Pages/Greatest Common Divisor of Strings.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Greatest Common Divisor of Strings.xcplaygroundpage/Contents.swift
@@ -9,11 +9,56 @@
  ```
  [**Link**](https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75)
  */
-
 import Foundation
 
 class Solution {
     func gcdOfStrings(_ str1: String, _ str2: String) -> String {
+        let l1 = str1.count
+        let l2 = str2.count
         
+        // Check if a substring of length 'len' can be a divisor of both strings
+        func isDivisor(_ len: Int) -> Bool {
+            if l1 % len == 0 && l2 % len == 0 {
+                let f1 = l1 / len
+                let f2 = l2 / len
+                
+                // calculate endIndex for both str1 and str2
+                let endIndex = str1.index(str1.startIndex, offsetBy: len)
+                let substring = String(str1[..<endIndex])
+                
+                // Check if repeating this substring reconstructs both strings
+                if String(repeating: substring, count: f1) == str1 && String(repeating: substring, count: f2) == str2 {
+                    return true
+                }
+            }
+            return false
+        }
+        
+        // Loop from the minimum length downwards to find the greatest divisor
+        for i in stride(from: min(l1, l2), through: 1, by: -1) {
+            if isDivisor(i) {
+                let endIndex = str1.index(str1.startIndex, offsetBy: i)
+                return String(str1[..<endIndex])
+            }
+        }
+        
+        // If no common divisor is found, return an empty string
+        return ""
     }
 }
+
+let str1 = "Lett", str2 = "ABAB"
+Solution().gcdOfStrings(str1, str2)
+
+/*:
+ Using Stride to run loop max to min
+ `for i in stride(from: min(l1, l2), through: 1, by: -1)`
+ 
+ 
+ GCD func
+ ```
+ func gcd(_ a: Int, _ b: Int) -> Int {
+    return b == 0 ? a : gcd(b, a % b)
+ }
+ ```
+ */
